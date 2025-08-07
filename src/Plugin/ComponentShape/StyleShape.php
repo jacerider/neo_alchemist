f376ea6bfb1e2b1cca6cc25951f9b3188d02e9a4
JaceRider <jacerider@gmail.com>
2025-08-07 16:55:49 -0500

    ✨ (StyleShape.php): introduce formWidgetAlter method to customize form options ♻️ (StyleShape.php): refactor getValue method to use ComponentShapeStyleAttribute for better attribute handling

--- src/Plugin/ComponentShape/StyleShape.php
+++ src/Plugin/ComponentShape/StyleShape.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\neo_alchemist\Plugin\ComponentShape;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\Core\Template\Attribute;
 use Drupal\neo_alchemist\Attribute\ComponentShape;
+use Drupal\neo_alchemist\ComponentShapeStyleAttribute;
 
 /**
  * Plugin implementation of the neo_component_shape.
@@ -34,9 +35,18 @@ class StyleShape extends StyleShapeBase {
   /**
    * {@inheritDoc}
    */
-  public function getPropValue(): Attribute {
-    $originalValue = parent::getPropValue();
-    $value = new Attribute();
+  protected function formWidgetAlter(array &$form, FormStateInterface $form_state): void {
+    if (isset($form['widget']['#options']['_none'])) {
+      $form['widget']['#options']['_none'] = $this->t('- Default -');
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getValue(): mixed {
+    $originalValue = parent::getValue();
+    $value = new ComponentShapeStyleAttribute([], $originalValue ?: NULL);
     if ($originalValue && isset($this->schema['styles'][$originalValue]['value'])) {
       $value->addClass($this->schema['styles'][$originalValue]['value']);
     }
