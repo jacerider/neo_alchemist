e28dea4d402273d93cede11fbd40f0241c5a0d9c
JaceRider <jacerider@gmail.com>
2025-08-07 16:57:22 -0500

    ✨ (TelephoneShape.php): introduce TelephoneShape plugin to handle telephone component shapes for better modularity and reusability in the application

---
+++ src/Plugin/ComponentShape/TelephoneShape.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentShape;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\neo_alchemist\Attribute\ComponentShape;
+use Drupal\neo_alchemist\ComponentShapePluginBase;
+
+/**
+ * Plugin implementation of the neo_component_shape.
+ */
+#[ComponentShape(
+  prop: 'telephone',
+  label: new TranslatableMarkup('Telephone'),
+  default_field_type: 'telephone',
+  default_field_widget: 'telephone_default',
+)]
+class TelephoneShape extends ComponentShapePluginBase {
+
+}
