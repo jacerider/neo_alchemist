4de22a4291ecd1c9a64e036c5e6c10c5aff1eacc
JaceRider <jacerider@gmail.com>
2025-08-07 16:55:38 -0500

    ♻️ (MarkupShape.php): refactor MarkupShape class to remove unused text format logic and simplify value adaptation 💡 (MarkupShape.php): add comments to clarify the value adaptation process and ensure proper rendering of string values

--- src/Plugin/ComponentShape/MarkupShape.php
+++ src/Plugin/ComponentShape/MarkupShape.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\neo_alchemist\Plugin\ComponentShape;
 
+use Drupal\Core\Render\Markup;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\neo_alchemist\Attribute\ComponentShape;
 use Drupal\neo_alchemist\ComponentShapePluginBase;
-use Drupal\neo_alchemist\ComponentShapeTextFormatPluginInterface;
 
 /**
  * Plugin implementation of the neo_component_shape.
@@ -20,45 +20,21 @@ use Drupal\neo_alchemist\ComponentShapeTextFormatPluginInterface;
   supports_field_props: ['string_long'],
   default_plugins: ['formatted_text'],
 )]
-class MarkupShape extends ComponentShapePluginBase implements ComponentShapeTextFormatPluginInterface {
-
-  /**
-   * The markup format.
-   *
-   * @var string
-   */
-  protected $textFormat = 'neo_simple';
-
-  /**
-   * Retrieves the markup format.
-   *
-   * @return string
-   *   The markup format.
-   */
-  protected function getTextFormat(): string {
-    return $this->textFormat;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function setTextFormat(string $textFormat): self {
-    $this->textFormat = $textFormat;
-    return $this;
-  }
+class MarkupShape extends ComponentShapePluginBase {
 
   /**
    * {@inheritDoc}
    */
   public function adaptValue(mixed $value): mixed {
-    if ($value) {
-      return [
-        '#type' => 'processed_text',
-        '#text' => $value,
-        '#format' => $this->getTextFormat(),
-      ];
+    if (is_array($value) && isset($value['value'])) {
+      // The #text property needs to be a string and not an array.
+      $value = $value['value'];
     }
-    return $value;
+    if (is_array($value)) {
+      return $value;
+    }
+    // This prop expects a string value, so we ensure it is rendered as such.
+    return Markup::create($value);
   }
 
 }
