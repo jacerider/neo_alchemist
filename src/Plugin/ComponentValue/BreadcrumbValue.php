095bd751a6b9487ec80e2da07fe94f3b83dc9c68
JaceRider <jacerider@gmail.com>
2025-08-07 16:57:26 -0500

    ✨ (BreadcrumbValue.php): introduce BreadcrumbValue plugin to provide breadcrumb links for component values, enhancing navigation and usability in the application.

---
+++ src/Plugin/ComponentValue/BreadcrumbValue.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentValue;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\neo_alchemist\Attribute\ComponentValue;
+use Drupal\neo_alchemist\ComponentShapePluginInterface;
+use Drupal\neo_alchemist\ComponentValuePluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the neo_component_value_provider.
+ */
+#[ComponentValue(
+  id: 'breadcrumb',
+  label: new TranslatableMarkup('Breadcrumb'),
+  description: new TranslatableMarkup('Use a breadcrumb to populate link fields.'),
+  group: 'providers',
+  weight: 10,
+  ref_types: [
+    'breadcrumb',
+  ],
+)]
+final class BreadcrumbValue extends ComponentValuePluginBase implements ContainerFactoryPluginInterface {
+
+  use DependencySerializationTrait;
+
+  /**
+   * The breadcrumb manager.
+   *
+   * @var \Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface
+   */
+  protected $breadcrumbManager;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    ComponentShapePluginInterface $shape,
+    array $configuration,
+    BreadcrumbBuilderInterface $breadcrumb_manager,
+    RouteMatchInterface $route_match,
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $shape, $configuration);
+    $this->breadcrumbManager = $breadcrumb_manager;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['shape'],
+      $configuration['settings'],
+      $container->get('breadcrumb'),
+      $container->get('current_route_match'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEditable(): bool {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'hide_current' => TRUE,
+    ];
+  }
+
+  /**
+   * Configuration form for the value provider plugin.
+   */
+  protected function configurationForm(array $form, FormStateInterface $form_state, array &$complete_form): array {
+
+    $form['hide_current'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide current'),
+      '#description' => $this->t('If checked, the current page will not be included in the breadcrumb.'),
+      '#default_value' => $this->configuration['hide_current'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function provideOverrideValue(mixed $value, mixed $defaultValue): mixed {
+    $breadcrumb = $this->breadcrumbManager->build($this->routeMatch);
+    $value = [];
+    $links = $breadcrumb->getLinks();
+    if ($links && $this->configuration['hide_current']) {
+      array_pop($links);
+    }
+    foreach ($links as $link) {
+      /** @var \Drupal\Core\Link $link */
+      $title = $link->getText();
+      $url = $link->getUrl();
+      if ($title && $url) {
+        $options = $url->getOptions();
+        $value[] = [
+          'title' => $link->getText(),
+          'url' => [
+            'title' => $link->getText(),
+            'uri' => $url->toUriString(),
+            'options' => $options,
+          ],
+        ];
+      }
+    }
+    return $value;
+  }
+
+}
