9757d9683ed68a7e0d2147c94b0a99071fa300e5
JaceRider <jacerider@gmail.com>
2025-08-07 16:56:49 -0500

    ✨ (ComponentFilterPluginEntityInterface.php): introduce new interface for neo_component_filter plugins to define entity operations and improve code structure

---
+++ src/ComponentFilterPluginEntityInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist;
+
+/**
+ * Interface for neo_component_filter plugins.
+ */
+interface ComponentFilterPluginEntityInterface extends ComponentFilterPluginInterface {
+
+  /**
+   * Gets the entity type ID this filter operates on.
+   *
+   * @return string
+   *   The entity type ID.
+   */
+  public function getEntityTypeId(): string;
+
+  /**
+   * Gets the entity bundle this filter operates on.
+   *
+   * @return string[]
+   *   The entity bundles.
+   */
+  public function getEntityBundles(): array;
+
+  /**
+   * Gets the entities that match the filter criteria.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   An array of entities that match the filter criteria.
+   */
+  public function getEntities(): array;
+
+}
