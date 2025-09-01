6b4a1f4b7a4df3788d1498c5ed5b2e7430bb974a
JaceRider <jacerider@gmail.com>
2025-09-01 13:35:30 -0500

    ✨ (EntityQueryPagerSlot.php): introduce new EntityQueryPagerSlot plugin to embed a pager for entity queries

---
+++ src/Plugin/ComponentSlot/EntityQueryPagerSlot.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentSlot;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\neo_alchemist\Attribute\ComponentSlot;
+use Drupal\neo_alchemist\ComponentInterface;
+use Drupal\neo_alchemist\ComponentSlotPluginBase;
+
+/**
+ * Plugin implementation of the neo_component_slot.
+ */
+#[ComponentSlot(
+  id: 'entity_query_pager',
+  label: new TranslatableMarkup('Entity Query | Pager'),
+  description: new TranslatableMarkup('Embed a pager for an entity query.'),
+)]
+final class EntityQueryPagerSlot extends ComponentSlotPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function toRenderable() {
+    return [
+      '#type' => 'pager',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(ComponentInterface $component) {
+    return $component->hasPropShapeWithPlugin('entity_query');
+  }
+
+}
