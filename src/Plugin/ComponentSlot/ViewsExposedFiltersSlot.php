942e8d812f23da8a9da11af284a142c654c7c0b5
JaceRider <jacerider@gmail.com>
2025-09-11 13:18:19 -0500

    ✨ (ViewsExposedFiltersSlot.php): introduce new ViewsExposedFiltersSlot plugin to embed exposed filters for views display

---
+++ src/Plugin/ComponentSlot/ViewsExposedFiltersSlot.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentSlot;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\neo_alchemist\Attribute\ComponentSlot;
+use Drupal\views\ViewExecutable;
+
+/**
+ * Plugin implementation of the neo_component_slot.
+ */
+#[ComponentSlot(
+  id: 'views_exposed_filters',
+  label: new TranslatableMarkup('Views | Exposed Filters'),
+  description: new TranslatableMarkup('Embed the exposed filters for a views display.'),
+)]
+final class ViewsExposedFiltersSlot extends ViewsSlotBase {
+
+  /**
+   * Convert the view to a renderable array.
+   */
+  protected function toViewsRenderable(ViewExecutable $view): array {
+    /** @var \Drupal\views\Plugin\views\exposed_form\ExposedFormPluginInterface $plugin */
+    $plugin = $view->display_handler->getPlugin('exposed_form');
+    return $plugin->renderExposedForm();
+  }
+
+}
