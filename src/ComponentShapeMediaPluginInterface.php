0f8a73389dce2b1e1f6c56537068a345c25f1ca1
JaceRider <jacerider@gmail.com>
2025-09-24 15:48:00 -0500

    ✨ (ComponentShapeMediaPluginInterface.php): add new methods for media preview and form handling to enhance media plugin functionality

--- src/ComponentShapeMediaPluginInterface.php
+++ src/ComponentShapeMediaPluginInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\neo_alchemist;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\media\MediaInterface;
 
 /**
@@ -29,5 +30,43 @@ interface ComponentShapeMediaPluginInterface {
    *   The value to be used for the component.
    */
   public function getValueFromMedia(MediaInterface $media): array;
+
+  /**
+   * Gets the default preview.
+   *
+   * Should be a renderable array.
+   *
+   * @return array|null
+   *   The image preview data or null if not applicable.
+   */
+  public function getDefaultPreview(): ?array;
+
+  /**
+   * Builds the media override form.
+   *
+   * @param array $form
+   *   The widget form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   * @param \Drupal\media\MediaInterface|null $media
+   *   The media entity, if available.
+   */
+  public function buildMediaOverrideForm(array $form, FormStateInterface $form_state, ?MediaInterface $media = NULL): array;
+
+  /**
+   * Massages the media override values using the plugin if available.
+   *
+   * @param array $values
+   *   The form values to be massaged.
+   * @param array $override_values
+   *   The override values from the override form.
+   * @param array $original_values
+   *   The original values.
+   * @param array $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The parent form state.
+   */
+  public function massageMediaOverrideValues(array &$values, array $override_values, array $original_values, array $form, FormStateInterface $form_state): void;
 
 }
