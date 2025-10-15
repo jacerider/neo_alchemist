76edbde3174aa8af6fe15cf7cd9365772a88fe9b
JaceRider <jacerider@gmail.com>
2025-10-15 09:32:20 -0500

    ✨ (StyleShape.php): introduce dependency injection for configuration and services to enhance testability and maintainability ♻️ (StyleShape.php): refactor getFieldOptions method to utilize configuration settings for style management ♻️ (StyleShape.php): refactor getPropValue method into preRenderValue for better clarity and separation of concerns

--- src/Plugin/ComponentShape/StyleShape.php
+++ src/Plugin/ComponentShape/StyleShape.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Drupal\neo_alchemist\Plugin\ComponentShape;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Template\Attribute;
 use Drupal\neo_alchemist\Attribute\ComponentShape;
 use Drupal\neo_alchemist\ComponentShapeStyleAttribute;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\WidgetPluginManager;
+use Drupal\Core\TypedData\TypedDataManagerInterface;
+use Drupal\neo_alchemist\ComponentInterface;
+use Drupal\neo_alchemist\ComponentShapePluginManager;
+use Drupal\neo_alchemist\ComponentValuePluginManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the neo_component_shape.
@@ -22,13 +31,71 @@ use Drupal\neo_alchemist\ComponentShapeStyleAttribute;
 class StyleShape extends StyleShapeBase {
 
   /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected ImmutableConfig $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    array $schema,
+    array $settings,
+    protected ComponentInterface $component,
+    protected EntityTypeManagerInterface $entityTypeManager,
+    protected TypedDataManagerInterface $typedDataManager,
+    protected WidgetPluginManager $widgetManager,
+    protected ComponentValuePluginManagerInterface $valueManager,
+    protected ConfigFactoryInterface $config_factory,
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $schema, $settings, $component, $entityTypeManager, $typedDataManager, $widgetManager, $valueManager);
+    $this->config = $this->config_factory->get('neo_alchemist.style_settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['schema'],
+      $configuration['settings'],
+      $configuration['component'],
+      $container->get('entity_type.manager'),
+      $container->get(TypedDataManagerInterface::class),
+      $container->get('plugin.manager.field.widget'),
+      $container->get('plugin.manager.neo_component_value'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
    * {@inheritDoc}
    */
   public function getFieldOptions(): ?array {
     if (array_key_exists('styles', $this->schema)) {
-      return array_map(function ($style) {
+      $values = array_map(function ($style) {
         return $style['label'] ?? 'Unnamed';
       }, $this->schema['styles']);
+      $settings = $this->config->get('styles.' . $this->getName());
+      if ($settings) {
+        $mode = $settings['mode'] ?? 'exclude';
+        $selected = array_filter($settings['values'] ?? []);
+        if ($selected) {
+          if ($mode === 'include') {
+            $values = array_intersect_key($values, array_flip($selected));
+          }
+          elseif ($mode === 'exclude') {
+            $values = array_diff_key($values, array_flip($selected));
+          }
+        }
+      }
+      return $values;
     }
     return [];
   }
@@ -38,23 +105,22 @@ class StyleShape extends StyleShapeBase {
    */
   protected function formWidgetAlter(array &$form, FormStateInterface $form_state): void {
     if (isset($form['widget']['#options']['_none'])) {
-      $form['widget']['#options']['_none'] = $this->t('- Default -');
+      $form['widget']['#options']['_none'] = $this->isRequired() ? $this->t('- Default -') : $this->t('- None -');
     }
   }
 
   /**
    * {@inheritDoc}
    */
-  public function getPropValue(Attribute $attributes): mixed {
-    $originalValue = parent::getPropValue($attributes);
-    $value = new ComponentShapeStyleAttribute([], $originalValue ?: NULL);
-    if ($originalValue && isset($this->schema['styles'][$originalValue]['value'])) {
-      $value->addClass($this->schema['styles'][$originalValue]['value']);
+  protected function preRenderValue(mixed $value, Attribute $attributes): mixed {
+    $finalValue = new ComponentShapeStyleAttribute([], $value ?: NULL);
+    if ($value && isset($this->schema['styles'][$value]['value'])) {
+      $finalValue->addClass($this->schema['styles'][$value]['value']);
     }
     if (array_key_exists('apply', $this->schema) && !empty($this->schema['apply'])) {
-      $attributes->merge($value);
+      $attributes->merge($finalValue);
     }
-    return $value;
+    return $finalValue;
   }
 
 }
