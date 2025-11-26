bb2dddfd90b2074799178f297b4b338a7c4e4e92
JaceRider <jacerider@gmail.com>
2025-11-26 16:43:18 -0600

    ✨ (ComponentInstanceInterface.php): add access method to check data value access for improved security and flexibility in permission handling

--- src/ComponentInstanceInterface.php
+++ src/ComponentInstanceInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\neo_alchemist;
 
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\neo_alchemist\Plugin\Field\FieldType\ComponentTreeItem;
 
 /**
@@ -97,5 +99,27 @@ interface ComponentInstanceInterface extends ComponentInterface {
    *   An associative array of values.
    */
   public function getValues(): array;
+
+  /**
+   * Checks data value access.
+   *
+   * @param string $operation
+   *   The operation to be performed.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   (optional) The user for which to check access, or NULL to check access
+   *   for the current user. Defaults to NULL.
+   * @param bool $return_as_object
+   *   (optional) Defaults to FALSE.
+   * @param \Drupal\neo_alchemist\Plugin\ComponentShape\ComponentShapePluginInterface|null $parentShape
+   *   (optional) The parent shape plugin, if any. Defaults to NULL.
+   *
+   * @return bool|\Drupal\Core\Access\AccessResultInterface
+   *   The access result. Returns a boolean if $return_as_object is FALSE (this
+   *   is the default) and otherwise an AccessResultInterface object.
+   *   When a boolean is returned, the result of AccessInterface::isAllowed() is
+   *   returned, i.e. TRUE means access is explicitly allowed, FALSE means
+   *   access is either explicitly forbidden or "no opinion".
+   */
+  public function access($operation, ?AccountInterface $account = NULL, $return_as_object = FALSE, ?ComponentShapePluginInterface $parentShape = NULL): bool|AccessResultInterface;
 
 }
