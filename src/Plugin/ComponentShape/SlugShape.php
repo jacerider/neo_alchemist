30735a9b2df341391f75e423eff8070627c2a3b0
JaceRider <jacerider@gmail.com>
2025-12-12 09:40:14 -0600

    ✨ (SlugShape.php): introduce SlugShape component shape plugin to handle unique slugs 📝 (SlugShape.php): add detailed documentation for methods and properties to improve code clarity

---
+++ src/Plugin/ComponentShape/SlugShape.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentShape;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Markup;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Template\Attribute;
+use Drupal\neo_alchemist\Attribute\ComponentShape;
+use Drupal\neo_alchemist\ComponentShapePluginBase;
+use Drupal\neo_alchemist\Drush\Generators\NeoComponentTwig;
+
+/**
+ * Plugin implementation of the neo_component_shape.
+ */
+#[ComponentShape(
+  prop: 'slug',
+  label: new TranslatableMarkup('Slug'),
+  default_field_type: 'string',
+  default_field_widget: 'slug',
+)]
+class SlugShape extends ComponentShapePluginBase {
+
+  /**
+   * Holds already used slug values to ensure uniqueness.
+   *
+   * @var array
+   */
+  protected static $slugs = [];
+
+  /**
+   * {@inheritDoc}
+   */
+  public function init(): self {
+    parent::init();
+    $this->getOptionDefault()->setAccess(FALSE);
+    $this->getOptionEmpty()->setAccess(FALSE);
+    return $this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function formWidgetAlter(array &$form, FormStateInterface $form_state): void {
+    $form['widget'][0]['value']['#field_prefix'] = '#';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function preRenderValue(mixed $value, Attribute $attributes): mixed {
+    if ($value) {
+      if (in_array($value, self::$slugs)) {
+        $suffix = 2;
+        while (in_array($value . '-' . $suffix, self::$slugs)) {
+          $suffix++;
+        }
+        $value .= '-' . $suffix;
+      }
+      self::$slugs[] = $value;
+    }
+    return $value;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function onGenerateTwig(NeoComponentTwig $twig) {
+    parent::onGenerateTwig($twig);
+    $twig->setContent('<div>{{' . $twig->getName() . '}}</div>');
+    return $twig;
+  }
+
+}
