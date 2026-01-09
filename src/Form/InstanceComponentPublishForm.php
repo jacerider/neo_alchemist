2fa9a09e81886fbe8735e0bcff2d53433ea1b996
JaceRider <jacerider@gmail.com>
2026-01-09 09:40:39 -0600

    ✨ (InstanceComponentPublishForm.php): add moderation state handling to the form to improve component publishing functionality 📝 (InstanceComponentPublishForm.php): update text in confirmation messages to reflect saving instead of publishing for clarity

--- src/Form/InstanceComponentPublishForm.php
+++ src/Form/InstanceComponentPublishForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\neo_alchemist\Form;
 
 use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Entity\SynchronizableInterface;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -34,6 +35,30 @@ class InstanceComponentPublishForm extends EntityConfirmFormBase {
    */
   public function form(array $form, FormStateInterface $form_state) {
     $this->fieldItem = $form_state->get('fieldItem');
+
+    if ($this->entity->hasField('moderation_state')) {
+      /** @var \Drupal\Core\Entity\Entity\EntityFormDisplay $formDisplay */
+      $formDisplay = $this->entityTypeManager
+        ->getStorage('entity_form_display')
+        ->load($this->entity->getEntityTypeId() . '.' . $this->entity->bundle() . '.default');
+      $component = $formDisplay->getComponent('moderation_state');
+      if ($component) {
+        // Get the widget plugin manager.
+        $widget_manager = \Drupal::service('plugin.manager.field.widget');
+        // Create the widget instance.
+        $widget = $widget_manager->getInstance([
+          'field_definition' => $this->entity->getFieldDefinition('moderation_state'),
+          'form_mode' => 'default',
+          'configuration' => $component,
+        ]);
+        $form['#parents'] = [];
+        // Build the widget form.
+        $items = $this->entity->get('moderation_state');
+        $form['moderation_state'] = $widget->form($items, $form, $form_state);
+        $form['moderation_state']['#access'] = $items->access('edit');
+        $form['moderation_state']['#weight'] = 100;
+      }
+    }
     return $form;
   }
 
@@ -41,7 +66,7 @@ class InstanceComponentPublishForm extends EntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getQuestion() {
-    return $this->t('Publish the components on %label: %field_label?', [
+    return $this->t('Save the components on %label: %field_label?', [
       '%label' => $this->entity->label(),
       '%field_label' => $this->fieldItem->getFieldDefinition()->getLabel(),
     ]);
@@ -58,14 +83,14 @@ class InstanceComponentPublishForm extends EntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getConfirmText() {
-    return $this->t('Publish');
+    return $this->t('Save');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getDescription() {
-    return $this->t('Are you sure you want to publish this layout and all of its components?');
+    return $this->t('Are you sure you want to save this layout and all of its components?');
   }
 
   /**
@@ -74,6 +99,19 @@ class InstanceComponentPublishForm extends EntityConfirmFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $fieldItem = $this->fieldItem->enforceAsDraft(FALSE);
     $form_state->setRedirectUrl($this->entity->toUrl());
+    $moderationState = $form_state->getValue(['moderation_state', 0, 'value']);
+    if ($moderationState) {
+      if ($this->entity->get('moderation_state')->value === $moderationState) {
+        // Skip new revision creation if the moderation state is not changing.
+        if ($this->entity instanceof SynchronizableInterface) {
+          $this->entity->setSyncing(TRUE);
+        }
+      }
+      $this->entity->set('moderation_state', $moderationState);
+      if ($this->entity->getEntityType()->hasLinkTemplate('latest-version') && !$this->entity->isDefaultRevision()) {
+        $form_state->setRedirectUrl($this->entity->toUrl('latest-version'));
+      }
+    }
     $result = $fieldItem->saveComponents();
     $this->messenger()->addStatus($this->t('Components have been published successfully on %label: %field_label.', [
       '%label' => $this->entity->label(),
