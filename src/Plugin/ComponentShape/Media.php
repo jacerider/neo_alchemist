fc6fb2227aafd5aae830fe4353998d5b5538a7db
JaceRider <jacerider@gmail.com>
2026-04-20 10:28:08 -0500

    Add generic media shape.

---
+++ src/Plugin/ComponentShape/Media.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\neo_alchemist\Plugin\ComponentShape;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\media\MediaInterface;
+use Drupal\neo_alchemist\Attribute\ComponentShape;
+use Drupal\neo_alchemist\Drush\Generators\NeoComponentPropGeneratorInterface;
+use DrupalCodeGenerator\InputOutput\Interviewer;
+
+/**
+ * Plugin implementation of the neo_component_shape.
+ */
+#[ComponentShape(
+  prop: 'media',
+  label: new TranslatableMarkup('Media'),
+  default_plugins: ['media'],
+)]
+class Media extends MediaShapeBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected bool $optionDefaultInitValue = FALSE;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getSupportedMediaTypes(): array {
+    return $this->getSchema()['media_types'] ?? ['image'];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function isFieldItemEmpty(): bool {
+    $value = $this->fieldItem->getValue();
+    if (!empty($value['entity_id'])) {
+      return FALSE;
+    }
+    return parent::isFieldItemEmpty();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getValueFromMedia(MediaInterface $media): array {
+    $source = $media->getSource();
+    return [
+      'entity_id' => $media->id(),
+      'src' => $source->getSourceFieldValue($media),
+      'title' => $media->label(),
+      'render' => $this->entityTypeManager->getViewBuilder('media')->view($media),
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function onGeneration(array &$prop, array $vars, Interviewer $ir, NeoComponentPropGeneratorInterface $generator, array $parents) {
+    $parents[$prop['name']] = $prop['title'];
+    $parentLabels = implode(' → ', ['Root'] + $parents);
+
+    $mediaTypes = \Drupal::entityTypeManager()->getStorage('media_type')->loadMultiple();
+    $choices = [];
+    foreach ($mediaTypes as $id => $mediaType) {
+      $choices[$id] = $mediaType->label();
+    }
+    if (empty($choices)) {
+      return;
+    }
+
+    $default = isset($choices['image']) ? 'image' : array_key_first($choices);
+    $selected = $ir->choice(\sprintf('%s: Allowed media types (comma-separated)', $parentLabels), $choices, $default, TRUE);
+    $prop['media_types'] = array_values((array) $selected);
+  }
+
+}
